337ef92230868097b6a21cc1a77287f8a0fb727a
01jegan <117276582+01jegan@users.noreply.github.com>
2023-02-16 09:20:34 +0530

    update

---
+++ hotel.js
@@ -0,0 +1,295 @@
+let foodList=[
+
+		{
+			id:1,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/checlpeshnx4porwdi7s",
+			name:"Balaajee Bhavan-T Nagar",
+			cusine:"North Indian, Chinese, Biryani, Chaat, Desserts,",
+			rating:4.2,
+			spa:"|",
+			time:"20 min",
+			spac:"|",
+			rate:"$ 399",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+
+		},
+
+		{
+			id:2,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ny8kj6b4mltw140wil1l",
+			name:"Sangeetha Veg Restaurant",
+			cusine:"Chettinad, North Indian, Chinese, South Indian",
+			rating:4.1,
+			spac:"|",
+			time:"20 min",
+			spa:"|",
+			rate:"$ 399",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+		},
+		{
+			id:3,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rlphdadmoq38bvaq3baw",
+			name:"Halal Kattu Biryani",
+			cusine:"Biryani, Indian, Chinese, Chettinad, Barbecue,",
+			rating:4.2,
+			spa:"|",
+			time:"20 min",
+			spac:"|",
+			rate:"$ 399",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+		},
+		{
+			id:4,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/jxmjqxkuxgqiktbrf77m",
+			name:"Meat and Eat",
+			cusine:"North Indian, Chinese, Biryani, Chaat, Desserts,",
+			rating:4.2,
+			spa:"|",
+			time:"20 min",
+			spac:"|",
+			rate:"$ 399",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+		},
+		{
+			id:5,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/jngyuv5qzuxa7vhwjngy",
+			name:"Domino's Pizza",
+			cusine:"North Indian, Chinese, Biryani, Chaat, Desserts,",
+			rating:4.2,
+			spa:"|",
+			time:"20 min",
+			spac:"|",
+			rate:"$ 399",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+		},
+		{
+			id:6,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ajqmhxi85slzxntfny3h",
+			name:"Pizza Lounge",
+			cusine:"Tibetan, Healthy Food, Asian, Chinese, Snacks, Continental, Desserts, Beverages",
+			rating:3.5,
+			spa:"|",
+			time:"33 MINS",
+			spac:"|",
+			rate:"₹200 FOR TWO",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+		},
+
+		{
+			id:7,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bdcd233971b7c81bf77e1fa4471280eb",
+			name:"KFC",
+			cusine:"Burgers, Biryani, American, Snacks, Fast Food",
+			rating:3.9,
+			spa:"|",
+			time:"36 MINS",
+			spac:"|",
+			rate:"₹400 FOR TWO",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+
+		},
+
+		{
+			id:8,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/c9c2a0f3e5885d96c8260ff2f34cb190",
+			name:"Zaitoon Signature",
+			cusine:"Barbecue, Tandoor, Indian, Kerala, Continental, Chinese, Juices, Desserts",
+			rating:4.3,
+			spac:"|",
+			time:"29 MINS",
+			spa:"|",
+			rate:"₹600 FOR TWO",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+		},
+		{
+			id:9,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/aab4bc89ce4991a11400733e48ec21d1",
+			name:"Chicking",
+			cusine:"Snacks, Grill, Fast Food, American, Pizzas, Mexican, Beverages",
+			rating:3.8,
+			spa:"|",
+			time:"51 MINS",
+			spac:"|",
+			rate:"₹599 FOR TWO",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+		},
+		{
+			id:10,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/0e8f93a56b93e43d7627c2c21bc1106c",
+			name:"Wendy's",
+			cusine:"American, Burgers, Fast Food, Beverages, Snacks",
+			rating:3.7,
+			spa:"|",
+			time:"36 MINS",
+			spac:"|",
+			rate:"₹200 FOR TWO",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+		},
+		{
+			id:11,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wywfr7xh0mx3r07mjcgh",
+			name:"Chicking",
+			cusine:"Snacks, Grill, Fast Food, American, Pizzas, Mexican, Beverages",
+			rating:3.8,
+			spa:"|",
+			time:"51 MINS",
+			spac:"|",
+			rate:"₹599 FOR TWO",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+		},
+		{
+			id:12,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/9a01f2935fcb5ae05426db8264062102",
+			name:"	Istah-Mediterranean Way",
+			cusine:"Arabian, Biryani, Mediterranean, Lebanese, Snacks, Beverages",
+			rating:4.1,
+			spa:"|",
+			time:"20 MINS",
+			spac:"|",
+			rate:"₹250 FOR TWO",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+		},
+		{
+			id:13,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/affnh0fjqay8bvxyovi8",
+			name:"Arabian Grills",
+			cusine:"Arabian, Grill, North Indian, Biryani",
+			rating:3.8,
+			spa:"|",
+			time:"20 MINS",
+			spac:"|",
+			rate:"₹150 FOR TWO",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+		},
+		{
+			id:14,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bmjt5bmw2h20b3uc2zsm",
+			name:"Savoury Sea Shell",
+			cusine:"Arabian, Indian, Chinese",
+			rating:4.3,
+			spa:"|",
+			time:"32 min",
+			spac:"|",
+			rate:"$ 399  FOR TWO",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+		},
+		{
+			id:15,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/snuiscl9oddarseimjz0",
+			name:"	zam zam fast food",
+			cusine:"Indian, Chinese",
+			rating:4.0,
+			spa:"|",
+			time:"33 min",
+			spac:"|",
+			rate:"$ 399 FOR TWO",
+			offer:"30% off on all orders",
+			view:"Quick View",
+		},
+		{
+			id:16,
+			imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/opqi0he3eokqxqw5xi6c",
+			name:"	Riyas Biriyani",
+			cusine:"Indian,",
+			rating:3.9,
+			spa:"|",
+			time:"20 min",
+			spac:"|",
+			rate:"$ 399",
+			offer:"50% off | Use WELCOME50",
+			view:"Quick View",
+		},
+		
+	]
+	let cont=document.querySelector(".container");
+	let cards=document.createElement("div");
+	cards.classList.add("row");
+	cont.appendChild(cards)
+
+foodList.forEach(function(e){
+
+	let div=document.createElement("div");
+	div.classList.add("col");
+	cards.appendChild(div)
+
+	let div2=document.createElement("div");
+	div2.classList.add("col-card");
+	div.appendChild(div2);
+
+	let img=document.createElement("img");
+	img.src=e.imgurl;
+	div2.appendChild(img);
+
+	let head=document.createElement("h2");
+	head.style.margin="10px";
+	head.innerHTML=e.name;
+
+	div2.appendChild(head);
+
+	let para=document.createElement("p");
+
+	para.innerHTML=e.cusine;
+	para.style.margin="10px";
+	div2.appendChild(para);
+
+
+
+	let div3=document.createElement("div");
+	div3.classList.add("col-menu");
+	para.appendChild(div3);
+
+	let para2=document.createElement("p");
+	para2.innerHTML=`<iconify-icon icon="ic:outline-star" class="ic"></iconify-icon>${e.rating}`;
+	para2.style.padding="5px";
+	div3.appendChild(para2);
+	para2.style.background="lightgreen";
+
+	let para6=document.createElement("p");
+	para6.innerHTML=e.spa;
+	div3.appendChild(para6);
+
+	let para3=document.createElement("p");
+	para3.innerHTML=e.time;
+	div3.appendChild(para3);
+
+	let para7=document.createElement("p");
+	para7.innerHTML=e.spac;
+	div3.appendChild(para7);
+
+
+	let para4=document.createElement("p");
+	para4.innerHTML=e.rate;
+	div3.appendChild(para4);
+
+	let para5=document.createElement("p");
+	para5.innerHTML=`<iconify-icon class="icon" icon="bxs:offer"></iconify-icon>${e.offer}`;
+	para5.style.textAlign="center";
+	para5.style.borderTop="1px solid black";
+	para5.style.paddingTop="5px";
+	div2.appendChild(para5);
+
+	let div4=document.createElement("div");
+	div4.classList.add("mass");
+	para5.appendChild(div4);
+
+	let para8=document.createElement("h3");
+	para8.innerHTML=e.view;
+	div4.appendChild(para8);
+});
+	
+
+	console.log(cont);
